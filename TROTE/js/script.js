18402637f54b73014e4c4a1e6b622a4464a77b31
Leonardo Augusto Dionisio <leonardodionizio14@gmail.com>
2025-03-27 20:07:01 -0300

    commit

--- TROTE/js/script.js
+++ TROTE/js/script.js
@@ -1,11 +1,15 @@
 function calcular(){
     //criar variaveis
-    let mascote, homenagem
+    let mascote, homenagem, leite
     mascote = document.getElementById("mascote").value
     
     homenagem = document.getElementById("homenagem").value
    
     let soma = Number(mascote) + Number(homenagem)
     
+    leite = document.getElementById("leite").value
+
+    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite))
+
     document.getElementById("soma").innerHTML = `a soma é ${soma}`
-}
+}0
